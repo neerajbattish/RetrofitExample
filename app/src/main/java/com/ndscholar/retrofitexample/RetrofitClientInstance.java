0b9304793f4c2347543a9d98bd98b8c8beececb3
Neeraj Battish <neerajbattish@yahoo.com>
2019-10-10 11:20:44 +0530

    First commit of this project

---
+++ app/src/main/java/com/ndscholar/retrofitexample/RetrofitClientInstance.java
@@ -0,0 +1,24 @@
+package com.ndscholar.retrofitexample;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+
+    private static Retrofit retrofit;
+    private static final String Base_URL="https://jsonplaceholder.typicode.com";
+
+    public static Retrofit getRetrofitInstance()
+    {
+        if(retrofit==null)
+        {
+
+
+            retrofit=new  Retrofit().newBuilder()
+                    .baseUrl(Base_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
